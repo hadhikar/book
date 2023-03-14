14eefa4840fd6e46476d3af4852832a74a24bd6f
Hari Adhikari <hadhikar@insight.com>
2023-03-14 10:22:59 -0400

    adidng new project

---
+++ BulkyBook/Migrations/20230310155711_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BulkyBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    category_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    display_order = table.Column<int>(type: "integer", nullable: false),
+                    created_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.category_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "category");
+        }
+    }
+}
---
+++ BulkyBook/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    [Table("category")]
+    public class Category
+    {
+        [Column("category_id")]
+        public int CategoryId { get; set; }
+
+        [Column("name")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Column("display_order")]
+        public int DisplayOrder { get; set; }
+
+        [Column("created_date_time")]
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
+
